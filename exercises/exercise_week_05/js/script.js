0a490c447732cc9e25e749dcba191d607bdb6d65
Felipe Dangond <felipe_dangond@hotmail.com>
2017-02-20 06:20:38 -0500

    added assignment 05
    …

---
+++ exercises/exercise_week_05/js/script.js
@@ -0,0 +1,18 @@
+/*
+
+Title of project
+Author's name
+
+Description of this script in the context of the project
+
+*/
+
+$(document).ready(function() {
+  responsiveVoice.on
+
+  // Write code in here that will run when the document is loaded
+setTimeout(function) () {
+responsiveVoice.speak("hello world");
+}, 1000
+
+});
